e89e63a4facccf1bdc5af17cb4c0b2e917aaa844
Daniel Purtov <daniel.purtov@gmail.com>
2023-11-23 20:11:43 +0100

    feat: Migrate to Vite 5

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite';
 import { getManifest } from './src/manifest.ts';
 import vue from '@vitejs/plugin-vue';
-import webExtension from '@samrum/vite-plugin-web-extension';
+import webExtension from 'vite-plugin-web-extension';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
     return {
-      // @ts-expect-error error
-      plugins: [vue(), webExtension({ manifest: getManifest() })],
-        // eslint-disable-next-line unicorn/relative-url-style, @typescript-eslint/naming-convention
+        plugins: [vue(), webExtension({ manifest: getManifest })],
+        build: { target: 'ESNext' },
+        // eslint-disable-next-line unicorn/relative-url-style
         resolve: { alias: { '~': new URL('./src', import.meta.url).pathname } },
     };
 });
