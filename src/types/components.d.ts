0425abefe036b72addf523628aec19f11d806189
Kamikaze <timo1322@gmail.com>
2023-12-15 22:34:08 +0100

    Fixed prettier errors due to new version

--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -4,11 +4,13 @@ export {};
 
 declare module 'vue' {
     export interface GlobalComponents {
-        Bar: typeof import('./components/Bar.vue')['default'];
-        BookmarkBars: typeof import('./components/BookmarksBars.vue')['default'];
-        Create: typeof import('./components/Create.vue')['default'];
-        Edit: typeof import('./components/Edit.vue')['default'];
-        Extension: typeof import('./components/Extension.vue')['default'];
-        Modal: typeof import('./components/Modal.vue')['default'];
+        /* eslint-disable prettier/prettier */
+        Bar: (typeof import('./components/Bar.vue'))['default'];
+        BookmarkBars: (typeof import('./components/BookmarksBars.vue'))['default'];
+        Create: (typeof import('./components/Create.vue'))['default'];
+        Edit: (typeof import('./components/Edit.vue'))['default'];
+        Extension: (typeof import('./components/Extension.vue'))['default'];
+        Modal: (typeof import('./components/Modal.vue'))['default'];
+        /* eslint-enable prettier/prettier */
     }
 }
