a833ed989730aa2ed81c06979332aaaf0ab019c6
Kamikaze <timo1322@gmail.com>
2023-12-15 22:38:53 +0100

    Removed eslint-disable rules
    The Editor still shows errors but linter and tests passes

--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -4,13 +4,11 @@ export {};
 
 declare module 'vue' {
     export interface GlobalComponents {
-        /* eslint-disable prettier/prettier */
         Bar: (typeof import('./components/Bar.vue'))['default'];
         BookmarkBars: (typeof import('./components/BookmarksBars.vue'))['default'];
         Create: (typeof import('./components/Create.vue'))['default'];
         Edit: (typeof import('./components/Edit.vue'))['default'];
         Extension: (typeof import('./components/Extension.vue'))['default'];
         Modal: (typeof import('./components/Modal.vue'))['default'];
-        /* eslint-enable prettier/prettier */
     }
 }
