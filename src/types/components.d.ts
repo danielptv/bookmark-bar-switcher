53d3af11402331805c023e4dec1f51964ff48c95
Daniel Purtov <daniel.purtov@gmail.com>
2024-02-16 18:41:31 +0100

    style: Fix ESLint finding

--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -4,11 +4,11 @@ export {};
 
 declare module 'vue' {
     export interface GlobalComponents {
-        Bar: typeof import('./components/Bar.vue')['default'];
-        BookmarkBars: typeof import('./components/BookmarksBars.vue')['default'];
-        Create: typeof import('./components/Create.vue')['default'];
-        Edit: typeof import('./components/Edit.vue')['default'];
-        Extension: typeof import('./components/Extension.vue')['default'];
-        Modal: typeof import('./components/Modal.vue')['default'];
+        Bar: (typeof import('./components/Bar.vue'))['default'];
+        BookmarkBars: (typeof import('./components/BookmarksBars.vue'))['default'];
+        Create: (typeof import('./components/Create.vue'))['default'];
+        Edit: (typeof import('./components/Edit.vue'))['default'];
+        Extension: (typeof import('./components/Extension.vue'))['default'];
+        Modal: (typeof import('./components/Modal.vue'))['default'];
     }
 }
